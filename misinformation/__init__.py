0c5f7168277eb4942542d034291b2a465968f628
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-07-13 13:54:19 +0200

    Move find_files into utility module

--- misinformation/__init__.py
+++ misinformation/__init__.py
@@ -5,4 +5,5 @@ from importlib import metadata
 __version__ = metadata.version(__package__)
 del metadata
 
-from misinformation.faces import explore_face_recognition, find_files
+from misinformation.faces import explore_face_recognition
+from misinformation.utils import find_files
--- misinformation/faces.py
+++ misinformation/faces.py
@@ -1,38 +1,8 @@
-import glob
 import ipywidgets
-import os
 
 from IPython.display import display
 from deepface import DeepFace
 from retinaface import RetinaFace
-
-
-def find_files(path=None, pattern="*.png", recursive=True, limit=20):
-    """Find image files on the file system
-
-    :param path:
-        The base directory where we are looking for the images. Defaults
-        to None, which uses the XDG data directory if set or the current
-        working directory otherwise.
-    :param pattern:
-        The naming pattern that the filename should match. Defaults to
-        "*.png". Can be used to allow other patterns or to only include
-        specific prefixes or suffixes.
-    :param recursive:
-        Whether to recurse into subdirectories.
-    :param limit:
-        The maximum number of images to be found. Defaults to 20.
-        To return all images, set to None.
-    """
-    if path is None:
-        path = os.environ.get("XDG_DATA_HOME", ".")
-
-    result = list(glob.glob(f"{path}/{pattern}", recursive=recursive))
-
-    if limit is not None:
-        result = result[:limit]
-
-    return result
 
 
 def facial_expression_analysis(img_path):
