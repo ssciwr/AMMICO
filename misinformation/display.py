995cfec9234d739443ac03fdfa64bc285d937e0d
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2022-08-07 20:48:21 +0200

    dict handling down to and from analysis routines (#19)

--- misinformation/display.py
+++ misinformation/display.py
@@ -17,15 +17,18 @@ class JSONContainer:
         return self._data
 
 
-def explore_analysis(image_paths, identify="faces"):
+def explore_analysis(mydict, identify="faces"):
     # dictionary mapping the type of analysis to be explored
     identify_dict = {
         "faces": faces.facial_expression_analysis,
         "text-on-image": text.detect_text,
     }
+    # create a list containing the image ids for the widget
+    # image_paths = [mydict[key]["filename"] for key in mydict.keys()]
+    image_ids = [key for key in mydict.keys()]
     # Create an image selector widget
     image_select = ipywidgets.Select(
-        options=image_paths, layout=ipywidgets.Layout(width="20%"), rows=20
+        options=image_ids, layout=ipywidgets.Layout(width="20%"), rows=20
     )
 
     # Set up the facial recognition output widget
@@ -44,14 +47,18 @@ def explore_analysis(image_paths, identify="faces"):
         output.clear_output()
 
         # Create the new content
-        image_widget.children = (ipywidgets.Image.from_file(image_select.value),)
+        image_widget.children = (
+            ipywidgets.Image.from_file(mydict[image_select.value]["filename"]),
+        )
 
         # This output widget absorbes print statements that are messing with
         # the widget output and cannot be disabled through the API.
         with faces.NocatchOutput():
-            analysis = identify_dict[identify](image_select.value)
+            mydict[image_select.value] = identify_dict[identify](
+                mydict[image_select.value]
+            )
         with output:
-            display(JSONContainer(analysis))
+            display(JSONContainer(mydict[image_select.value]))
 
     # Register the handler and trigger it immediately
     image_select.observe(switch, names=("value",), type="change")
