9ae96b9794cdcd047b1b4d8775f6f253b55c8ca5
iulusoy <inga.ulusoy@uni-heidelberg.de>
2022-09-07 15:54:26 +0200

    account for older python

--- misinformation/__init__.py
+++ misinformation/__init__.py
@@ -1,4 +1,8 @@
-from importlib import metadata
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata  # type: ignore
 
 
 # Export the version defined in project metadata
