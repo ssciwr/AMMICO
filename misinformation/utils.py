ef305ee94cf4e0c7c40096099e8a73afa2958b85
Dominic Kempf <dokempf@users.noreply.github.com>
2022-12-13 11:33:34 +0100

    Fix ID generation for relative paths (#35)
    So far, there was implicit assumption on the used paths being absolute.
    
    Co-authored-by: Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>

--- misinformation/utils.py
+++ misinformation/utils.py
@@ -75,8 +75,8 @@ def find_files(path=None, pattern="*.png", recursive=True, limit=20):
 def initialize_dict(filelist: list) -> dict:
     mydict = {}
     for img_path in filelist:
-        id_ = img_path.split(".")[0].split("/")[-1]
-        mydict[id_] = {"filename": img_path}
+        id = os.path.splitext(os.path.basename(img_path))[0]
+        mydict[id] = {"filename": img_path}
     return mydict
 
 
