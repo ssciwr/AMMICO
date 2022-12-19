25ed5881a12e062d1b853f32489a09385935231e
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2022-12-19 15:03:05 +0100

    Text 2 (#42)
    * start with translate
    
    * translate and clean - notebook
    
    * spacy model in requirements
    
    * translate in module
    
    * clean in module
    
    * upload coverage only for ubuntu
    
    * update ubuntu version on runner
    
    * update dependencies
    
    * start tests for text
    
    * skip gcv test
    
    * fix age
    
    * more text tests
    
    * more text tests
    
    * add comment
    
    * test translation
    
    * fix numpy version; add reference data for trans
    
    * use utf-8 for windows

---
+++ misinformation/test/test_text.py
@@ -0,0 +1,77 @@
+import os
+import pytest
+import spacy
+import misinformation.text as tt
+
+TESTDICT = {
+    "IMG_3755": {
+        "filename": "./test/data/IMG_3755.jpg",
+    },
+    "IMG_3756": {
+        "filename": "./test/data/IMG_3756.jpg",
+    },
+    "IMG_3757": {
+        "filename": "./test/data/IMG_3757.jpg",
+    },
+}
+
+LANGUAGES = ["de", "om", "en"]
+
+os.environ[
+    "GOOGLE_APPLICATION_CREDENTIALS"
+] = "../data/seismic-bonfire-329406-412821a70264.json"
+
+
+def test_TextDetector():
+    for item in TESTDICT:
+        test_obj = tt.TextDetector(TESTDICT[item])
+        assert test_obj.subdict["text"] is None
+        assert test_obj.subdict["text_language"] is None
+        assert test_obj.subdict["text_english"] is None
+        assert test_obj.subdict["text_cleaned"] is None
+
+
+@pytest.mark.gcv
+def test_get_text_from_image():
+    for item in TESTDICT:
+        test_obj = tt.TextDetector(TESTDICT[item])
+        test_obj.get_text_from_image()
+        ref_file = "./test/data/text_" + item + ".txt"
+        with open(ref_file, "r", encoding="utf8") as file:
+            reference_text = file.read()
+        assert test_obj.subdict["text"] == reference_text
+
+
+def test_translate_text():
+    for item, lang in zip(TESTDICT, LANGUAGES):
+        test_obj = tt.TextDetector(TESTDICT[item])
+        ref_file = "./test/data/text_" + item + ".txt"
+        trans_file = "./test/data/text_translated_" + item + ".txt"
+        with open(ref_file, "r", encoding="utf8") as file:
+            reference_text = file.read()
+        with open(trans_file, "r", encoding="utf8") as file:
+            translated_text = file.read()
+        test_obj.subdict["text"] = reference_text
+        test_obj.translate_text()
+        assert test_obj.subdict["text_language"] == lang
+        assert test_obj.subdict["text_english"] == translated_text
+
+
+def test_init_spacy():
+    test_obj = tt.TextDetector(TESTDICT["IMG_3755"])
+    ref_file = "./test/data/text_IMG_3755.txt"
+    with open(ref_file, "r") as file:
+        reference_text = file.read()
+    test_obj.subdict["text_english"] = reference_text
+    test_obj._init_spacy()
+    assert isinstance(test_obj.doc, spacy.tokens.doc.Doc)
+
+
+def test_clean_text():
+    nlp = spacy.load("en_core_web_md")
+    doc = nlp("I like cats and fjejg")
+    test_obj = tt.TextDetector(TESTDICT["IMG_3755"])
+    test_obj.doc = doc
+    test_obj.clean_text()
+    result = "I like cats and"
+    assert test_obj.subdict["text_clean"] == result
--- misinformation/text.py
+++ misinformation/text.py
@@ -1,4 +1,6 @@
 from google.cloud import vision
+from googletrans import Translator
+import spacy
 import io
 from misinformation import utils
 
@@ -7,29 +9,37 @@ class TextDetector(utils.AnalysisMethod):
     def __init__(self, subdict: dict) -> None:
         super().__init__(subdict)
         self.subdict.update(self.set_keys())
+        self.translator = Translator()
+        # spacy load should be separaate method with error if model not found / dynamic download
+        self.nlp = spacy.load("en_core_web_md")
 
     def set_keys(self) -> dict:
-        params = {"text": None}
+        params = {
+            "text": None,
+            "text_language": None,
+            "text_english": None,
+            "text_cleaned": None,
+        }
         return params
 
     def analyse_image(self):
-        """Detects text on the image."""
+        self.get_text_from_image()
+        self.translate_text()
+        self._init_spacy()
+        self.clean_text()
+        return self.subdict
 
+    def get_text_from_image(self):
+        """Detects text on the image."""
         path = self.subdict["filename"]
         client = vision.ImageAnnotatorClient()
-
         with io.open(path, "rb") as image_file:
             content = image_file.read()
-
         image = vision.Image(content=content)
-
         response = client.text_detection(image=image)
-        texts = response.text_annotations
+        texts = response.text_annotations[0].description
         # here check if text was found
-        self.subdict = {"text": []}
-        for text in texts:
-            self.subdict["text"].append(text.description)
-
+        self.subdict = {"text": texts}
         if response.error.message:
             raise Exception(
                 "{}\nFor more info on error messages, check: "
@@ -37,4 +47,21 @@ class TextDetector(utils.AnalysisMethod):
                     response.error.message
                 )
             )
-        return self.subdict
+
+    def translate_text(self):
+        translated = self.translator.translate(self.subdict["text"])
+        self.subdict["text_language"] = translated.src
+        self.subdict["text_english"] = translated.text
+
+    def _init_spacy(self):
+        """Generate spacy doc object."""
+        self.doc = self.nlp(self.subdict["text_english"])
+
+    def clean_text(self):
+        """Clean the text from unrecognized words and any numbers."""
+        templist = []
+        for token in self.doc:
+            templist.append(
+                token.text
+            ) if token.pos_ != "NUM" and token.has_vector else None
+        self.subdict["text_clean"] = " ".join(templist).rstrip().lstrip()
