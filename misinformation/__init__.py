f0a96bae860a0336b13c1394681ec625902f6c97
iulusoy <inga.ulusoy@uni-heidelberg.de>
2023-02-15 03:31:39 -0800

    skip import on windows

--- misinformation/__init__.py
+++ misinformation/__init__.py
@@ -8,11 +8,3 @@ except ImportError:
 # Export the version defined in project metadata
 __version__ = metadata.version(__package__)
 del metadata
-
-from misinformation.display import explore_analysis
-from misinformation.utils import (
-    find_files,
-    initialize_dict,
-    append_data_to_dict,
-    dump_df,
-)
