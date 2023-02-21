40435a73a15709bb95db12f83d90c5240df09b65
Petr Andriushchenko <pitandmind@gmail.com>
2023-02-21 12:23:50 +0100

    fix clip models

--- misinformation/multimodal_search.py
+++ misinformation/multimodal_search.py
@@ -1,5 +1,8 @@
 from misinformation.utils import AnalysisMethod
 import torch
+import torch.nn.functional as Func
+import requests
+import lavis
 from PIL import Image
 from IPython.display import display
 from lavis.models import load_model_and_preprocess
@@ -89,20 +92,38 @@ class MultimodalSearch(AnalysisMethod):
                     )
                     for ten in images_tensors
                 ]
+                features_image_stacked = torch.stack(
+                    [
+                        feat.image_embeds_proj[:, 0, :].squeeze(0)
+                        for feat in features_image
+                    ]
+                )
+        elif model_type in ("clip_base", "clip_rn50", "clip_vitl14"):
+            features_image = [
+                model.extract_features({"image": ten}) for ten in images_tensors
+            ]
+            features_image_stacked = torch.stack(
+                [
+                    Func.normalize(feat.float(), dim=-1).squeeze(0)
+                    for feat in features_image
+                ]
+            )
         else:
             features_image = [
                 model.extract_features({"image": ten, "text_input": ""}, mode="image")
                 for ten in images_tensors
             ]
-
-        features_image_stacked = torch.stack(
-            [feat.image_embeds_proj[:, 0, :].squeeze(0) for feat in features_image]
-        )
-
+            features_image_stacked = torch.stack(
+                [feat.image_embeds_proj[:, 0, :].squeeze(0) for feat in features_image]
+            )
         return features_image_stacked
 
-    def save_tensors(features_image_stacked, name="saved_features_image.pt"):
-        with open(name, "wb") as f:
+    def save_tensors(
+        model_type, features_image_stacked, name="saved_features_image.pt"
+    ):
+        with open(
+            str(len(features_image_stacked)) + "_" + model_type + "_" + name, "wb"
+        ) as f:
             torch.save(features_image_stacked, f)
         return name
 
@@ -117,8 +138,16 @@ class MultimodalSearch(AnalysisMethod):
         return features_text
 
     def parsing_images(self, model_type):
+
+        if model_type in ("clip_base", "clip_rn50", "clip_vitl14"):
+            path_to_lib = lavis.__file__[:-11] + "models/clip_models/"
+            url = "https://raw.githubusercontent.com/salesforce/LAVIS/main/lavis/models/clip_models/bpe_simple_vocab_16e6.txt.gz"
+            r = requests.get(url, allow_redirects=False)
+            open(path_to_lib + "bpe_simple_vocab_16e6.txt.gz", "wb").write(r.content)
+
         image_keys = sorted(self.keys())
         image_names = [self[k]["filename"] for k in image_keys]
+
         (
             model,
             vis_processors,
@@ -132,7 +161,7 @@ class MultimodalSearch(AnalysisMethod):
         features_image_stacked = MultimodalSearch.extract_image_features(
             model, images_tensors, model_type
         )
-        MultimodalSearch.save_tensors(features_image_stacked)
+        MultimodalSearch.save_tensors(model_type, features_image_stacked)
 
         return (
             model,
@@ -142,6 +171,73 @@ class MultimodalSearch(AnalysisMethod):
             image_names,
             features_image_stacked,
         )
+
+    def querys_processing(
+        self, search_query, model, txt_processors, vis_processors, model_type
+    ):
+        for query in search_query:
+            if not (len(query) == 1) and (query in ("image", "text_input")):
+                raise SyntaxError(
+                    'Each querry must contain either an "image" or a "text_input"'
+                )
+        multi_sample = []
+        for query in search_query:
+            if "text_input" in query.keys():
+                text_processing = txt_processors["eval"](query["text_input"])
+                images_tensors = ""
+            elif "image" in query.keys():
+                _, images_tensors = MultimodalSearch.read_and_process_images(
+                    [query["image"]], vis_processors
+                )
+                text_processing = ""
+            multi_sample.append(
+                {"image": images_tensors, "text_input": text_processing}
+            )
+
+        if model_type in ("clip_base", "clip_rn50", "clip_vitl14"):
+            multi_features_query = []
+            for query in multi_sample:
+                if query["image"] == "":
+                    features = model.extract_features(
+                        {"text_input": query["text_input"]}
+                    )
+
+                    features_squeeze = features.squeeze(0).to(
+                        MultimodalSearch.multimodal_device
+                    )
+                    multi_features_query.append(
+                        Func.normalize(features_squeeze, dim=-1)
+                    )
+                if query["text_input"] == "":
+                    multi_features_query.append(
+                        MultimodalSearch.extract_image_features(
+                            model, query["image"], model_type
+                        )
+                    )
+
+        else:
+            multi_features_query = []
+            for query in multi_sample:
+                if query["image"] == "":
+                    features = model.extract_features(query, mode="text")
+                    features_squeeze = (
+                        features.text_embeds_proj[:, 0, :]
+                        .squeeze(0)
+                        .to(MultimodalSearch.multimodal_device)
+                    )
+                    multi_features_query.append(features_squeeze)
+                if query["text_input"] == "":
+                    multi_features_query.append(
+                        MultimodalSearch.extract_image_features(
+                            model, query["image"], model_type
+                        )
+                    )
+
+        multi_features_stacked = torch.stack(
+            [query.squeeze(0) for query in multi_features_query]
+        ).to(MultimodalSearch.multimodal_device)
+
+        return multi_features_stacked
 
     def multimodal_search(
         self,
@@ -155,46 +251,9 @@ class MultimodalSearch(AnalysisMethod):
     ):
         features_image_stacked.to(MultimodalSearch.multimodal_device)
 
-        for query in search_query:
-            if len(query) != 1:
-                raise SyntaxError(
-                    'Each querry must contain either an "image" or a "text_input"'
-                )
-
-        multi_sample = []
-        for query in search_query:
-            if "text_input" in query.keys():
-                text_processing = txt_processors["eval"](query["text_input"])
-                image_processing = ""
-            elif "image" in query.keys():
-                _, image_processing = MultimodalSearch.read_and_process_images(
-                    [query["image"]], vis_processors
-                )
-                text_processing = ""
-            multi_sample.append(
-                {"image": image_processing, "text_input": text_processing}
-            )
-
-        multi_features_query = []
-        for query in multi_sample:
-            if query["image"] == "":
-                features = model.extract_features(query, mode="text")
-                features_squeeze = (
-                    features.text_embeds_proj[:, 0, :]
-                    .squeeze(0)
-                    .to(MultimodalSearch.multimodal_device)
-                )
-                multi_features_query.append(features_squeeze)
-            if query["text_input"] == "":
-                multi_features_query.append(
-                    MultimodalSearch.extract_image_features(
-                        model, query["image"], model_type
-                    )
-                )
-
-        multi_features_stacked = torch.stack(
-            [query.squeeze(0) for query in multi_features_query]
-        ).to(MultimodalSearch.multimodal_device)
+        multi_features_stacked = MultimodalSearch.querys_processing(
+            self, search_query, model, txt_processors, vis_processors, model_type
+        )
 
         similarity = features_image_stacked @ multi_features_stacked.t()
         sorted_lists = [
