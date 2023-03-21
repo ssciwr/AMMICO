35f7a52e5f6ca76a81c074bee43e0cb1c6a4455d
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2023-03-21 11:35:37 +0100

    first doc updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "myst_parser"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "myst_parser", "nbsphinx"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
