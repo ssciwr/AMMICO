97b3cebe0a7bce224b2bfa516acbf07957dfbb8c
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2023-03-24 14:16:18 +0100

    update display test outputs

--- misinformation/test/test_display.py
+++ misinformation/test/test_display.py
@@ -1,27 +1,29 @@
 import json
-
-# import misinformation.display as misinf_display
+import misinformation.display as misinf_display
 import pytest
 
-misinf_display = pytest.importorskip("misinformation.display")
 
-
-def test_explore_analysis_faces():
-    mydict = {"IMG_2746": {"filename": "./test/data/IMG_2746.png"}}
+def test_explore_analysis_faces(get_path):
+    mydict = {"IMG_2746": {"filename": get_path + "IMG_2746.png"}}
     misinf_display.explore_analysis(mydict, identify="faces")
-    with open("./test/data/example_faces.json", "r") as file:
+    with open(get_path + "example_faces.json", "r") as file:
         outs = json.load(file)
-
+    mydict["IMG_2746"].pop("filename", None)
     for im_key in mydict.keys():
         sub_dict = mydict[im_key]
         for key in sub_dict.keys():
             assert sub_dict[key] == outs[key]
 
 
-def test_explore_analysis_objects():
-    mydict = {"IMG_2746": {"filename": "./test/data/IMG_2809.png"}}
+def test_explore_analysis_objects(get_path):
+    mydict = {"IMG_2809": {"filename": get_path + "IMG_2809.png"}}
     misinf_display.explore_analysis(mydict, identify="objects")
-    with open("./test/data/example_analysis_objects.json", "r") as file:
+    with open(get_path + "example_analysis_objects.json", "r") as file:
         outs = json.load(file)
-
-    assert str(mydict) == str(outs)
+    mydict["IMG_2809"].pop("filename", None)
+    print(mydict)
+    for im_key in mydict.keys():
+        sub_dict = mydict[im_key]
+        for key in sub_dict.keys():
+            print(outs[key])
+            assert sub_dict[key] == outs[key]
