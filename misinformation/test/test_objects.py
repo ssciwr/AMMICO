a5c43b64882996fa8cdef1eba2ce6787b28eded7
Petr Andriushchenko <pitandmind@gmail.com>
2023-03-30 10:33:05 +0200

    Test debugging (#62)
    * deleted lavis from utils
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fixed test_objects
    
    * added 'not gcv' to CI
    
    * fixed multimodal search and summary tests
    
    * disable doc build on PR for now
    
    * restrict ipywidgets version to avoid dummycomm error
    
    * limit deepface version
    
    * original repositories for retinaface lavis
    
    * update gcv test results
    
    * update display test outputs
    
    * update test env
    
    * run all tests
    
    * wo xdist to avoid segfault
    
    * remove widgets ref
    
    * skip long-running tests
    
    * skip long
    
    * verbose codecov upload
    
    * refactor summary test 2
    
    * finish summary test refactor
    
    * reduce memory overhead of SummaryDetector
    
    * remove VQA models from self
    
    * remove VQA models from self
    
    * update notebook for changes
    
    * update notebook for changes
    
    * fixed multimodal search tests
    
    * fixed tests in multimodal search after precommit
    
    * run all tests
    
    * update doc notebook for summary changes
    
    * skip long-running multimodal
    
    * exclude blip2 from testing
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>

--- misinformation/test/test_objects.py
+++ misinformation/test/test_objects.py
@@ -31,6 +31,7 @@ def test_analyse_image_cvlib(get_path):
 
     with open(get_path + JSON_1, "r") as file:
         out_dict = json.load(file)
+    out_dict["filename"] = get_path + out_dict["filename"]
     for key in mydict.keys():
         assert mydict[key] == out_dict[key]
 
@@ -56,10 +57,11 @@ def test_init_default_objects():
 
 def test_analyse_image_from_file_cvlib(get_path):
     file_path = get_path + TEST_IMAGE_1
-    objs = ob_cvlib.ObjectCVLib().analyse_image_from_file(get_path + file_path)
+    objs = ob_cvlib.ObjectCVLib().analyse_image_from_file(file_path)
 
     with open(get_path + JSON_1, "r") as file:
         out_dict = json.load(file)
+    out_dict["filename"] = get_path + out_dict["filename"]
     for key in objs.keys():
         assert objs[key] == out_dict[key]
 
@@ -86,5 +88,6 @@ def test_analyse_image(get_path):
     ob.ObjectDetector(mydict).analyse_image()
     with open(get_path + JSON_1, "r") as file:
         out_dict = json.load(file)
+    out_dict["filename"] = get_path + out_dict["filename"]
 
     assert str(mydict) == str(out_dict)
