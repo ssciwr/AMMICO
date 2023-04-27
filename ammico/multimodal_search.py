0df232277444af28d2c2abbd738ca5e32f5d14a2
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2023-04-27 13:25:24 +0200

    use iterator as a default value in lambda

--- ammico/multimodal_search.py
+++ ammico/multimodal_search.py
@@ -17,7 +17,6 @@ from lavis.processors import load_processor
 class MultimodalSearch(AnalysisMethod):
     def __init__(self, subdict: dict) -> None:
         super().__init__(subdict)
-        # self.subdict.update(self.set_keys())
 
     multimodal_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -319,9 +318,12 @@ class MultimodalSearch(AnalysisMethod):
             )
 
         similarity = features_image_stacked @ multi_features_stacked.t()
-        # similarity_soft_max = torch.nn.Softmax(dim=0)(similarity / 0.01)
         sorted_lists = [
-            sorted(range(len(similarity)), key=lambda k: similarity[k, i], reverse=True)
+            sorted(
+                range(len(similarity)),
+                key=lambda k, value=i: similarity[k, value],
+                reverse=True,
+            )
             for i in range(len(similarity[0]))
         ]
         places = [[item.index(i) for i in range(len(item))] for item in sorted_lists]
