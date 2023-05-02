cb39fbd635fed9723ad09e521f9bd018bf11f4d8
GwydionJon <gwy.daskalakis@googlemail.com>
2023-05-02 13:38:47 +0200

    added variable for img path in tests

--- misinformation/test/test_display.py
+++ misinformation/test/test_display.py
@@ -28,14 +28,17 @@ def test_explore_analysis_objects(get_path):
 
 
 def test_AnalysisExplorer(get_path):
+    path_img_1 = get_path + "IMG_2809.png"
+    path_img_2 = get_path + "IMG_2746.png"
+
     mydict = {
-        "IMG_2809": {"filename": get_path + "IMG_2809.png"},
-        "IMG_2746": {"filename": get_path + "IMG_2746.png"},
+        "IMG_2809": {"filename": path_img_1},
+        "IMG_2746": {"filename": path_img_2},
     }
 
     all_options_dict = {
-        get_path + "IMG_2809.png": "IMG_2809",
-        get_path + "IMG_2746.png": "IMG_2746",
+        path_img_1: "IMG_2809",
+        path_img_2: "IMG_2746",
     }
 
     analysis_explorer_faces = misinf_display.AnalysisExplorer(mydict, identify="faces")
@@ -43,16 +46,16 @@ def test_AnalysisExplorer(get_path):
         mydict, identify="objects"
     )
 
-    analysis_explorer_faces.update_picture(get_path + "IMG_2809.png")
-    analysis_explorer_objects.update_picture(get_path + "IMG_2746.png")
+    analysis_explorer_faces.update_picture(path_img_1)
+    analysis_explorer_objects.update_picture(path_img_2)
 
     assert analysis_explorer_objects.update_picture(None) is None
 
     analysis_explorer_faces._right_output_analysis(
-        None, ["something", "faces"], all_options_dict, get_path + "IMG_2809.png"
+        None, ["something", "faces"], all_options_dict, path_img_1
     )
     analysis_explorer_objects._right_output_analysis(
-        None, ["something", "objects"], all_options_dict, get_path + "IMG_2746.png"
+        None, ["something", "objects"], all_options_dict, path_img_2
     )
 
     with pytest.raises(EnvironmentError):
