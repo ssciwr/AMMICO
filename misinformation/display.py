827eda417a56b1efc17ee079d4ac12a3ede21adf
GwydionJon <gwy.daskalakis@googlemail.com>
2023-05-02 13:22:22 +0200

    put das explorer app in new class

--- misinformation/display.py
+++ misinformation/display.py
@@ -27,33 +27,85 @@ class JSONContainer:
         return self._data
 
 
-def explore_analysis(mydict, identify="faces", port=8050):
-    # this function is used to get a broad overview before commiting to a full analysis.
-    app = jupyter_dash.JupyterDash(__name__)
+class AnalysisExplorer:
+    def __init__(self, mydict, identify="faces") -> None:
+        self.app = jupyter_dash.JupyterDash(__name__)
+        self.mydict = mydict
+        self.theme = {
+            "scheme": "monokai",
+            "author": "wimer hazenberg (http://www.monokai.nl)",
+            "base00": "#272822",
+            "base01": "#383830",
+            "base02": "#49483e",
+            "base03": "#75715e",
+            "base04": "#a59f85",
+            "base05": "#f8f8f2",
+            "base06": "#f5f4f1",
+            "base07": "#f9f8f5",
+            "base08": "#f92672",
+            "base09": "#fd971f",
+            "base0A": "#f4bf75",
+            "base0B": "#a6e22e",
+            "base0C": "#a1efe4",
+            "base0D": "#66d9ef",
+            "base0E": "#ae81ff",
+            "base0F": "#cc6633",
+        }
 
-    theme = {
-        "scheme": "monokai",
-        "author": "wimer hazenberg (http://www.monokai.nl)",
-        "base00": "#272822",
-        "base01": "#383830",
-        "base02": "#49483e",
-        "base03": "#75715e",
-        "base04": "#a59f85",
-        "base05": "#f8f8f2",
-        "base06": "#f5f4f1",
-        "base07": "#f9f8f5",
-        "base08": "#f92672",
-        "base09": "#fd971f",
-        "base0A": "#f4bf75",
-        "base0B": "#a6e22e",
-        "base0C": "#a1efe4",
-        "base0D": "#66d9ef",
-        "base0E": "#ae81ff",
-        "base0F": "#cc6633",
-    }
+        # setup the layout
+        app_layout = html.Div(
+            [
+                # top
+                html.Div(
+                    ["Identify: ", identify, self._top_file_explorer(mydict)],
+                    id="Div_top",
+                    style={
+                        "width": "30%",
+                        # "display": "inline-block",
+                    },
+                ),
+                # middle
+                html.Div(
+                    [self._middle_picture_frame()],
+                    id="Div_middle",
+                    style={
+                        "width": "60%",
+                        "display": "inline-block",
+                        "verticalAlign": "top",
+                    },
+                ),
+                # right
+                html.Div(
+                    [self._right_output_json()],
+                    id="Div_right",
+                    style={
+                        "width": "30%",
+                        "display": "inline-block",
+                        "verticalAlign": "top",
+                    },
+                ),
+            ],
+            style={"width": "80%", "display": "inline-block"},
+        )
+        self.app.layout = app_layout
+        # add callbacks to app
+        self.app.callback(
+            Output("img_middle_picture_id", "src"),
+            Input("left_select_id", "value"),
+            prevent_initial_call=True,
+        )(self.update_picture)
+
+        self.app.callback(
+            Output("right_json_viewer", "data"),
+            Input("img_middle_picture_id", "src"),
+            State("Div_top", "children"),
+            State("left_select_id", "options"),
+            State("left_select_id", "value"),
+            prevent_initial_call=True,
+        )(self._right_output_analysis)
 
     # I split the different sections into subfunctions for better clarity
-    def _top_file_explorer(mydict):
+    def _top_file_explorer(self, mydict):
         # initilizes the dropdown that selects which file is to be analyzed.
         left_layout = html.Div(
             [
@@ -65,7 +117,7 @@ def explore_analysis(mydict, identify="faces", port=8050):
         )
         return left_layout
 
-    def _middle_picture_frame():
+    def _middle_picture_frame(self):
         # This just holds the image
         middle_layout = html.Div(
             [
@@ -79,7 +131,7 @@ def explore_analysis(mydict, identify="faces", port=8050):
         )
         return middle_layout
 
-    def _right_output_json():
+    def _right_output_json(self):
         # provides the json viewer for the analysis output.
         right_layout = html.Div(
             [
@@ -92,7 +144,7 @@ def explore_analysis(mydict, identify="faces", port=8050):
                                     id="right_json_viewer",
                                     data={},
                                     max_depth=-1,
-                                    theme=theme,
+                                    theme=self.theme,
                                     invert_theme=True,
                                 )
                             ]
@@ -104,27 +156,23 @@ def explore_analysis(mydict, identify="faces", port=8050):
         )
         return right_layout
 
-    @app.callback(
-        Output("img_middle_picture_id", "src"),
-        Input("left_select_id", "value"),
-        prevent_initial_call=True,
-    )
-    def update_picture(img_path):
+    def run_server(self, port=8050):
+        if not is_interactive():
+            raise EnvironmentError(
+                "Dash server should only be called in interactive an interactive environment like jupyter notebooks."
+            )
+
+        self.app.run_server(debug=True, mode="inline", port=port)
+
+    # Dash callbacks
+    def update_picture(self, img_path):
         if img_path is not None:
             image = Image.open(img_path)
             return image
         else:
             return None
 
-    @app.callback(
-        Output("right_json_viewer", "data"),
-        Input("img_middle_picture_id", "src"),
-        State("Div_top", "children"),
-        State("left_select_id", "options"),
-        State("left_select_id", "value"),
-        prevent_initial_call=True,
-    )
-    def _right_output_analysis(image, div_top, all_options, current_value):
+    def _right_output_analysis(self, image, div_top, all_options, current_value):
         # calls the analysis function and returns the output
         identify_dict = {
             "faces": faces.EmotionDetector,
@@ -137,50 +185,5 @@ def explore_analysis(mydict, identify="faces", port=8050):
 
         identify_function = identify_dict[div_top[1]]
 
-        mydict[image_id] = identify_function(mydict[image_id]).analyse_image()
-        return mydict[image_id]
-
-    # setup the layout
-    app_layout = html.Div(
-        [
-            # top
-            html.Div(
-                ["Identify: ", identify, _top_file_explorer(mydict)],
-                id="Div_top",
-                style={
-                    "width": "30%",
-                    # "display": "inline-block",
-                },
-            ),
-            # middle
-            html.Div(
-                [_middle_picture_frame()],
-                id="Div_middle",
-                style={
-                    "width": "60%",
-                    "display": "inline-block",
-                    "verticalAlign": "top",
-                },
-            ),
-            # right
-            html.Div(
-                [_right_output_json()],
-                id="Div_right",
-                style={
-                    "width": "30%",
-                    "display": "inline-block",
-                    "verticalAlign": "top",
-                },
-            ),
-        ],
-        style={"width": "80%", "display": "inline-block"},
-    )
-    app.layout = app_layout
-
-    if not is_interactive():
-        raise EnvironmentError(
-            "Dash server should only be called in interactive an interactive environment like jupyter notebooks."
-        )
-
-    app.run_server(debug=True, mode="inline", port=port)
-    # return app
+        self.mydict[image_id] = identify_function(self.mydict[image_id]).analyse_image()
+        return self.mydict[image_id]
