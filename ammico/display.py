3b9b706948eb3f0e45dd208ae2883f2a1f0f50e9
Petr Andriushchenko <pitandmind@gmail.com>
2023-05-12 16:43:51 +0200

    checkout display to previous version for tesing

--- ammico/display.py
+++ ammico/display.py
@@ -1,16 +1,11 @@
+import ipywidgets
 from IPython.display import display
 
 import ammico.faces as faces
 import ammico.text as text
 import ammico.objects as objects
-from ammico.utils import is_interactive
 
 import ammico.summary as summary
-
-import dash_renderjson
-from dash import html, Input, Output, dcc, State
-import jupyter_dash
-from PIL import Image
 
 
 class JSONContainer:
@@ -27,163 +22,54 @@ class JSONContainer:
         return self._data
 
 
-class AnalysisExplorer:
-    def __init__(self, mydict, identify="faces") -> None:
-        self.app = jupyter_dash.JupyterDash(__name__)
-        self.mydict = mydict
-        self.identify = identify
-        self.theme = {
-            "scheme": "monokai",
-            "author": "wimer hazenberg (http://www.monokai.nl)",
-            "base00": "#272822",
-            "base01": "#383830",
-            "base02": "#49483e",
-            "base03": "#75715e",
-            "base04": "#a59f85",
-            "base05": "#f8f8f2",
-            "base06": "#f5f4f1",
-            "base07": "#f9f8f5",
-            "base08": "#f92672",
-            "base09": "#fd971f",
-            "base0A": "#f4bf75",
-            "base0B": "#a6e22e",
-            "base0C": "#a1efe4",
-            "base0D": "#66d9ef",
-            "base0E": "#ae81ff",
-            "base0F": "#cc6633",
-        }
+def explore_analysis(mydict, identify="faces"):
+    # dictionary mapping the type of analysis to be explored
+    identify_dict = {
+        "faces": faces.EmotionDetector,
+        "text-on-image": text.TextDetector,
+        "objects": objects.ObjectDetector,
+        "summary": summary.SummaryDetector,
+    }
+    # create a list containing the image ids for the widget
+    # image_paths = [mydict[key]["filename"] for key in mydict.keys()]
+    image_ids = [key for key in mydict.keys()]
+    # Create an image selector widget
+    image_select = ipywidgets.Select(
+        options=image_ids, layout=ipywidgets.Layout(width="20%"), rows=20
+    )
 
-        # setup the layout
-        app_layout = html.Div(
-            [
-                # top
-                html.Div(
-                    ["Identify: ", identify, self._top_file_explorer(mydict)],
-                    id="Div_top",
-                    style={
-                        "width": "30%",
-                        # "display": "inline-block",
-                    },
-                ),
-                # middle
-                html.Div(
-                    [self._middle_picture_frame()],
-                    id="Div_middle",
-                    style={
-                        "width": "60%",
-                        "display": "inline-block",
-                        "verticalAlign": "top",
-                    },
-                ),
-                # right
-                html.Div(
-                    [self._right_output_json()],
-                    id="Div_right",
-                    style={
-                        "width": "30%",
-                        "display": "inline-block",
-                        "verticalAlign": "top",
-                    },
-                ),
-            ],
-            style={"width": "80%", "display": "inline-block"},
+    # Set up the facial recognition output widget
+    output = ipywidgets.Output(layout=ipywidgets.Layout(width="30%"))
+
+    # Set up the image selection and display widget
+    image_widget = ipywidgets.Box(
+        children=[],
+        layout=ipywidgets.Layout(width="50%"),
+    )
+
+    # Register the tab switch logic
+    def switch(_):
+        # Clear existing output
+        image_widget.children = ()
+        output.clear_output()
+
+        # Create the new content
+        image_widget.children = (
+            ipywidgets.Image.from_file(mydict[image_select.value]["filename"]),
         )
-        self.app.layout = app_layout
-        # add callbacks to app
-        self.app.callback(
-            Output("img_middle_picture_id", "src"),
-            Input("left_select_id", "value"),
-            prevent_initial_call=True,
-        )(self.update_picture)
 
-        self.app.callback(
-            Output("right_json_viewer", "data"),
-            Input("img_middle_picture_id", "src"),
-            State("left_select_id", "options"),
-            State("left_select_id", "value"),
-            prevent_initial_call=True,
-        )(self._right_output_analysis)
+        # This output widget absorbes print statements that are messing with
+        # the widget output and cannot be disabled through the API.
+        with faces.NocatchOutput():
+            mydict[image_select.value] = identify_dict[identify](
+                mydict[image_select.value]
+            ).analyse_image()
+        with output:
+            display(JSONContainer(mydict[image_select.value]))
 
-    # I split the different sections into subfunctions for better clarity
-    def _top_file_explorer(self, mydict):
-        # initilizes the dropdown that selects which file is to be analyzed.
-        left_layout = html.Div(
-            [
-                dcc.Dropdown(
-                    options={value["filename"]: key for key, value in mydict.items()},
-                    id="left_select_id",
-                )
-            ]
-        )
-        return left_layout
+    # Register the handler and trigger it immediately
+    image_select.observe(switch, names=("value",), type="change")
+    switch(None)
 
-    def _middle_picture_frame(self):
-        # This just holds the image
-        middle_layout = html.Div(
-            [
-                html.Img(
-                    id="img_middle_picture_id",
-                    style={
-                        "width": "80%",
-                    },
-                )
-            ]
-        )
-        return middle_layout
-
-    def _right_output_json(self):
-        # provides the json viewer for the analysis output.
-        right_layout = html.Div(
-            [
-                dcc.Loading(
-                    id="loading-2",
-                    children=[
-                        html.Div(
-                            [
-                                dash_renderjson.DashRenderjson(
-                                    id="right_json_viewer",
-                                    data={},
-                                    max_depth=-1,
-                                    theme=self.theme,
-                                    invert_theme=True,
-                                )
-                            ]
-                        )
-                    ],
-                    type="circle",
-                )
-            ]
-        )
-        return right_layout
-
-    def run_server(self, port=8050):
-        if not is_interactive():
-            raise EnvironmentError(
-                "Dash server should only be called in interactive an interactive environment like jupyter notebooks."
-            )
-
-        self.app.run_server(debug=True, mode="inline", port=port)
-
-    # Dash callbacks
-    def update_picture(self, img_path):
-        if img_path is not None:
-            image = Image.open(img_path)
-            return image
-        else:
-            return None
-
-    def _right_output_analysis(self, image, all_options, current_value):
-        # calls the analysis function and returns the output
-        identify_dict = {
-            "faces": faces.EmotionDetector,
-            "text-on-image": text.TextDetector,
-            "objects": objects.ObjectDetector,
-            "summary": summary.SummaryDetector,
-        }
-        # get image ID from dropdown value, which is the filepath.
-        image_id = all_options[current_value]
-
-        identify_function = identify_dict[self.identify]
-
-        self.mydict[image_id] = identify_function(self.mydict[image_id]).analyse_image()
-        return self.mydict[image_id]
+    # Show the combined widget
+    return ipywidgets.HBox([image_select, image_widget, output])
