1d9e1338eaff5d5e97182d6738aff5f98da88058
GwydionJon <35529247+GwydionJon@users.noreply.github.com>
2023-06-22 13:58:49 +0200

    Improve colors expression (#80)
    * increased dash width
    
    * added new color_analysis notebook
    
    * added colorgram.py to dependencies
    
    * added first iteration of new color_check
    
    * added new version of color analysis
    
    * added webcolors to dependencies
    
    * added colormath
    
    * switched from colormath to colour-science
    
    * made delta_e algorithm user accessible
    
    * remove obsolete notebook
    
    * update docstrings and type hints
    
    * add color analysis module to API doc
    
    * renamed color_expressions to color_analysis
    
    * renamed test
    
    * updated color analysis notebook to adhere to the same style as other notebooks
    
    * updated test for new df orientation
    
    * refactored color analysis to comply with ammico workflow
    
    * updated color tests to comply with new class structure
    
    * added explanation to colors_analysis notebook
    
    * added class doc string
    
    * updated analysis explorer test to include empty image keyword as dash observer
    
    * fix typo and names, docstring and import
    
    * update doc and notebook explanation
    
    * add project url for pypi
    
    ---------
    
    Co-authored-by: Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>

--- ammico/display.py
+++ ammico/display.py
@@ -1,6 +1,7 @@
 import ammico.faces as faces
 import ammico.text as text
 import ammico.objects as objects
+import ammico.colors as colors
 from ammico.utils import is_interactive
 import ammico.summary as summary
 import dash_renderjson
@@ -45,7 +46,7 @@ class AnalysisExplorer:
             "base0F": "#cc6633",
         }
 
-        #  Setup the layout
+        # Setup the layout
         app_layout = html.Div(
             [
                 # Top
@@ -197,7 +198,9 @@ class AnalysisExplorer:
         else:
             return None
 
-    def _right_output_analysis(self, all_options: dict, current_value: str) -> dict:
+    def _right_output_analysis(
+        self, image, all_options: dict, current_value: str
+    ) -> dict:
         """Callback function to perform analysis on the selected image and return the output.
 
         Args:
@@ -212,6 +215,7 @@ class AnalysisExplorer:
             "text-on-image": text.TextDetector,
             "objects": objects.ObjectDetector,
             "summary": summary.SummaryDetector,
+            "colors": colors.ColorDetector,
         }
 
         # Get image ID from dropdown value, which is the filepath
