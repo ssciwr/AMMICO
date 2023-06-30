44a2e7201439f91bee3076f03891be04a2bb8e11
Petr Andriushchenko <pitandmind@gmail.com>
2023-06-30 17:34:43 +0200

    returned all tests to macos CI

--- ammico/test/test_objects.py
+++ ammico/test/test_objects.py
@@ -26,7 +26,7 @@ def test_objects_from_cvlib(default_objects):
     assert str(objects) == str(out_objects)
 
 
-@pytest.mark.skipif(sys.platform == "darwin", reason="segmentation fault on mac")
+#@pytest.mark.skipif(sys.platform == "darwin", reason="segmentation fault on mac")
 def test_analyse_image_cvlib(get_path):
     mydict = {"filename": get_path + TEST_IMAGE_1}
     ob_cvlib.ObjectCVLib().analyse_image(mydict)
@@ -57,7 +57,7 @@ def test_init_default_objects():
         assert init_objects[obj] == "no"
 
 
-@pytest.mark.skipif(sys.platform == "darwin", reason="segmentation fault on mac")
+#@pytest.mark.skipif(sys.platform == "darwin", reason="segmentation fault on mac")
 def test_analyse_image_from_file_cvlib(get_path):
     file_path = get_path + TEST_IMAGE_1
     objs = ob_cvlib.ObjectCVLib().analyse_image_from_file(file_path)
@@ -69,7 +69,7 @@ def test_analyse_image_from_file_cvlib(get_path):
         assert objs[key] == out_dict[key]
 
 
-@pytest.mark.skipif(sys.platform == "darwin", reason="segmentation fault on mac")
+#@pytest.mark.skipif(sys.platform == "darwin", reason="segmentation fault on mac")
 def test_detect_objects_cvlib(get_path):
     file_path = get_path + TEST_IMAGE_1
     objs = ob_cvlib.ObjectCVLib().detect_objects_cvlib(file_path)
