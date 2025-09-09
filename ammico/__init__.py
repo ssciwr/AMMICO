6768b8c273a0c6b85dc6e03568b95c5923b64cd1
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2025-09-09 15:18:49 +0200

    mainatin: remove more obsolete files, restructure pyproject.toml

--- ammico/__init__.py
+++ ammico/__init__.py
@@ -1,16 +1,16 @@
-try:
-    from importlib import metadata
-except ImportError:
-    # Running on pre-3.8 Python; use importlib-metadata package
-    import importlib_metadata as metadata  # type: ignore
+from importlib import metadata
 from ammico.display import AnalysisExplorer
 from ammico.faces import EmotionDetector, ethical_disclosure
 from ammico.text import TextDetector, TextAnalyzer, privacy_disclosure
 from ammico.utils import find_files, get_dataframe
 
 # Export the version defined in project metadata
-__version__ = metadata.version(__package__)
-del metadata
+try:
+    from importlib.metadata import version
+
+    __version__ = version("ammico")
+except ImportError:
+    __version__ = "unknown"
 
 __all__ = [
     "AnalysisExplorer",
